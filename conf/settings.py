2cdce938278c47eca29e41867d95bf68b2bc9582
Michael Dougherty <michael@lv11.co>
2013-08-16 23:45:43 -0700

    moved backend down to root

--- conf/settings.py
+++ conf/settings.py
@@ -1,7 +1,7 @@
 from os.path import *
 
 RAW_DATA_DIR = abspath(join(dirname(__file__), '../rawdata'))
-APP_DATA_DIR = abspath(join(dirname(__file__), '../../frontend/data'))
+APP_DATA_DIR = abspath(join(dirname(__file__), '../frontend/data'))
 
 SPATIALITE_ENCODING = 'UTF-8'
 SPATIALITE_SRID = 4326
